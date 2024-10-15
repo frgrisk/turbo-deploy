98d2e1f3fbbb4e6b5a2e8676f423cae5bc6a9bb2
lbazran <luqman.bazran@frgrisk.com>
2024-10-15 15:56:02 +0800

    fix: userData field initializes with empty string instead of array

--- client/src/app/create-deployment/create-deployment.component.ts
+++ client/src/app/create-deployment/create-deployment.component.ts
@@ -41,8 +41,8 @@ export class CreateDeploymentComponent implements OnInit {
       region: new FormControl('', [Validators.required]),
       ami: new FormControl('', [Validators.required]),
       serverSize: new FormControl('', [Validators.required]),
-      userData: new FormControl(''),
-      lifecycle: new FormControl(Lifecycle.ON_DEMAND, [Validators.required]),
+      userData: new FormControl([]),
+      lifecycle: new FormControl(Lifecycle.SPOT, [Validators.required]),
       ttlValue: new FormControl('', [Validators.min(1)]),
       ttlUnit: new FormControl(''),
     });
