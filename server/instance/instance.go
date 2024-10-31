43ca7d457bc184e489348f682796b8c32e5e930a
lbazran <luqman.bazran@frgrisk.com>
2024-10-31 13:37:04 +0800

    fix: userdata field in db has the wrong value when taking snapshot

--- server/instance/instance.go
+++ server/instance/instance.go
@@ -87,7 +87,7 @@ func GetDeployedInstances() ([]models.DeploymentResponse, error) {
 					AvailabilityZone: aws.ToString(instance.Placement.AvailabilityZone),
 					Lifecycle:        getLifecycle(instance.InstanceLifecycle),
 					Status:           string(instance.State.Name),
-					UserData:         strings.Split(getInstanceTagValue("UserData", instance.Tags), ","),
+					UserData:         splitUserData(getInstanceTagValue("UserData", instance.Tags)),
 				}
 
 				deployments = append(deployments, deployment)
@@ -98,6 +98,12 @@ func GetDeployedInstances() ([]models.DeploymentResponse, error) {
 	return deployments, nil
 }
 
+func splitUserData(userData string) []string {
+    if userData == "" {
+        return []string{}
+    }
+    return strings.Split(userData, ",")
+}
 func StartInstance(instanceID string) error {
 	input := &ec2.StartInstancesInput{
 		InstanceIds: []string{instanceID},
